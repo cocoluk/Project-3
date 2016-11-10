c39ea981b54441fe9ed7396328481584ec09a74d
Courtney Luk <courtneyluk@Courtney-Luks-MacBook-Pro-2.local>
2016-11-10 13:54:00 -0500

    updated bshw3 file

--- bshw3.py
+++ bshw3.py
@@ -8,12 +8,49 @@
 # 3) Replace any local images with the image I provided in media.  (You
 # must keep the image in a separate folder than your html code.
 
-import bs4
+import requests
+from bs4 import BeautifulSoup
+import urllib
+import re
 
-with open('BSI admissions _ University of Michigan School of Information.htm') as inf:
-	txt = inf.read()
-	soup = bs4.BeautifulSoup(txt)
-	
+base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, 'html.parser')
+
+soup = soup.prettify()
+
+
+soup = soup.replace('https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_192.jpg','///Users/courtneyluk/Desktop/Portfolio/me.png')
+soup = soup.replace('student','AMAZING student')
+#soup = soup.replace()
+
+#print(soup)
+
+html = open('final_file','w')
+html.write(soup)
+html.close()
+
+#add commit push
+
+
+#htmldoc = open('BSI admissions _ University of Michigan School of Information.htm','r+')
+#soup = BeautifulSoup(htmldoc, 'html.parser')
+#nice_soup = soup.prettify
+#print (soup)
+
+#for text_chunk in nice_soup.find_all('div', {'class':'field-item even'}, {'propert':'content:encoded'}):
+#	print (text_chunk.p)
+	#if text_chunk.h3:
+	#	print(text_chunk.h3)
+
+#html.write
+
+#with open('BSI admissions _ University of Michigan School of Information.htm') as inf:
+#	txt = inf.read()
+#	soup = bs4.BeautifulSoup(txt)
+
+#new_link = soup.new_tag
+
 
 
 
